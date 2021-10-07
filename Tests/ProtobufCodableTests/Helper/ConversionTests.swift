0f20653bd8b1999b20bae3dbb7848a002a24b37a
Christoph Hagen <christoph@spacemasters.eu>
2021-10-07 23:02:51 +0200

    Improve output for failed tests

--- Tests/ProtobufCodableTests/Helper/ConversionTests.swift
+++ Tests/ProtobufCodableTests/Helper/ConversionTests.swift
@@ -2,27 +2,53 @@ import Foundation
 import XCTest
 @testable import ProtobufCodable
 
+private func encode<T>(_ value: T) throws -> Data where T: Encodable {
+    do {
+        return try ProtobufEncoder().encode(value)
+    } catch {
+        print("Failed to encode \(value): \(error)")
+        throw error
+    }
+}
+
+private func decode<T>(_ data: Data) throws -> T where T: Decodable {
+    do {
+        return try ProtobufDecoder().decode(from: data)
+    } catch {
+        print("Failed to decode: \(error)\n\(data.bytes)")
+        throw error
+    }
+}
+
+private func deserialize<T>(_ type: T.Type, _ data: Data) throws -> T.ProtobufType where T: ProtobufComparable {
+    do {
+        return try T.ProtobufType(serializedData: data)
+    } catch {
+        print("Failed to deserialize: \(error)\n\(data.bytes)")
+        throw error
+    }
+}
+
 func roundTripProtobuf<T>(_ message: T) throws where T: ProtobufComparable {
     let protoData = try message.protobuf.serializedData()
-    let codableData = try ProtobufEncoder().encode(message)
-    
+    let codableData = try encode(message)
+
     // First round trip codable
-    let decodedCodable1: T = try ProtobufDecoder().decode(from: codableData)
+    let decodedCodable1: T = try decode(codableData)
     XCTAssertEqual(message, decodedCodable1)
 
     // Then decode the protobuf data
-    let decodedCodable2: T = try ProtobufDecoder().decode(from: protoData)
+    let decodedCodable2: T = try decode(protoData)
     XCTAssertEqual(message, decodedCodable2)
 
-    // Finale decode codable data as a protobuf
-    let decodedProtobuf = try T.ProtobufType(serializedData: codableData)
+    // Finally decode codable data as a protobuf
+    let decodedProtobuf = try deserialize(T.self, codableData)
     XCTAssertEqual(T.init(protoObject: decodedProtobuf), message)
 }
 
 func roundTripCodable<T>(type: T.Type = T.self, _ codable: T) throws where T: Codable, T: Equatable {
-    let data = try ProtobufEncoder().encode(codable)
-    print(data.bytes)
-    let decoded: T = try ProtobufDecoder().decode(from: data)
+    let data = try encode(codable)
+    let decoded: T = try decode(data)
 
     XCTAssertEqual(decoded, codable)
     if decoded != codable {
